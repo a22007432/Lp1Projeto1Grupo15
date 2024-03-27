895f73b85715c6ceb0c729b3c7f7b5676d1c839c
a22007432 <ssm.antonio@gmail.com>
2024-03-27 12:44:20 +0000

    tested SetBoardMethof

--- GaloDaVelha/Game.cs
+++ GaloDaVelha/Game.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,17 +11,37 @@ namespace GaloDaVelha
     /// </summary>
     public class Game
     {
-        private int[][] board = new int [4][]; 
+        private int[,] board = new int [4,4]; 
 
 
         
         //Possible method to start the game
         public void StartGame()
         {
+            SetBoard(board);
+            for(int i= 0; i < 4; i++)
+            {
+                for(int j = 0; j< 4; j ++)
+                {
+                    Console.WriteLine(board[i,j]);
+                }
+            }
             
         }
 
-        private string[] 
+        //Sets the array of the board
+        public static int[,] SetBoard(int[,] board)
+        {
+            for(int i= 0; i < 4; i++)
+            {
+                //rows should be all 0 but for testing they go from 1 to 4
+                for(int j = 0; j< 4; j ++)
+                {
+                    board[i,j] = j +1;
+                }
+            }
+            return board;
+        }
 
     }
 
