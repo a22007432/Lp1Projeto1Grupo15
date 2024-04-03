306bda9f53ca2f7502ff1b461e508ee6cf0615c2
a22007432 <ssm.antonio@gmail.com>
2024-04-03 13:54:07 +0100

    added test method for the layout

--- GaloDaVelha/Game.cs
+++ GaloDaVelha/Game.cs
@@ -44,7 +44,8 @@ namespace GaloDaVelha
         {
             
             
-            SetBoard(board);
+            SetDefaultBoard(board);
+            SetBoardTest(board);
 
             //testcode for chars
             /*Console.WriteLine(cLargeHole);
@@ -76,8 +77,12 @@ namespace GaloDaVelha
             
         }
 
-        //Sets the array of the board as 0
-        public static int[,] SetBoard(int[,] board)
+        /// <summary>
+        /// Sets the board array to 0 which means no piece
+        /// </summary>
+        /// <param name="board"><Array containing the values of the board>
+        /// <returns><Array containing the values of the board>
+        public static int[,] SetDefaultBoard(int[,] board)  
         {
             for(int i= 0; i < 4; i++)
             {
@@ -91,6 +96,36 @@ namespace GaloDaVelha
         }
 
         /// <summary>
+        /// This functions tests the board 
+        /// </summary>
+        /// <param name="board"><Array containing the values of the board>
+        /// <returns><Array containing the values of the board>
+        public static int[,] SetBoardTest(int[,] board)  
+        {
+            for(int i= 0; i < 4; i++)
+            {
+                if (i < 2)
+                {
+                    for(int j = 0; j< 4; j ++)
+                    {
+                        if (i == 0){board[i,j] = j+1;}
+                        else{board[i,j] = 5+j;}
+                    }
+                }
+                else
+                {
+                    for(int j = 0; j< 4; j ++)
+                    {
+                        if (i == 2){board[i,j] = j+1;}
+                        else{board[i,j] = 5+j;}
+                    }
+                }
+
+            }
+            return board; 
+        }
+
+        /// <summary>
         /// This method returns a char from an Enum using an int as reference
         /// </summary>
         /// <param name="pieceInt"><This is the int that decides the piece char>
@@ -98,37 +133,36 @@ namespace GaloDaVelha
         public char SetPiece(int pieceInt)
         {
        
-
             char tmpPiece = '\0';
             switch(pieceInt)
             {
                  //This case might work better as default 
                 case 0:
-                    tmpPiece = (char) Pieces.Null;
+                    tmpPiece = (char) Pieces.Null; //X
                     break;
                 case 1:
-                    tmpPiece = (char) Pieces.BigSquare;
+                    tmpPiece = (char) Pieces.BigSquare;//☐
                     break;
                 case 2:
-                    tmpPiece = (char) Pieces.SmallSquare;
+                    tmpPiece = (char) Pieces.SmallSquare;//◇
                     break;
                 case 3:
-                    tmpPiece = (char) Pieces.BigSquareH;
+                    tmpPiece = (char) Pieces.BigSquareH;//☒ 
                     break;
                 case 4:
-                    tmpPiece = (char) Pieces.SmallSquareH;
+                    tmpPiece = (char) Pieces.SmallSquareH;//◈
                     break;
                 case 5:
-                    tmpPiece = (char) Pieces.BigCircle;
+                    tmpPiece = (char) Pieces.BigCircle;//○
                     break;
                 case 6:
-                    tmpPiece = (char) Pieces.SmallCircle;
+                    tmpPiece = (char) Pieces.SmallCircle;//◦
                     break;
                 case 7:
-                    tmpPiece = (char) Pieces.BigCircleH;
+                    tmpPiece = (char) Pieces.BigCircleH;//◎
                     break;
                 case 8:
-                    tmpPiece = (char) Pieces.SmallCircleH;
+                    tmpPiece = (char) Pieces.SmallCircleH;//☉
                     break;
                 
 
@@ -142,8 +176,9 @@ namespace GaloDaVelha
 
         /// <summary>
         /// Prints the board using the int values stored in the board array
+        /// Should be callled after every move
         /// </summary>
-        /// <param name="currentBoard"><The current board array>
+        ///<param name="currentBoard"><Array containing the values of the board>
         public void PrintBoard(int[,] currentBoard)
         {   //Idea For how to print the board
             /*Console.WriteLine($"|  ||  ||  ||  |");
--- GaloDaVelha/Pieces.cs
+++ GaloDaVelha/Pieces.cs
@@ -4,7 +4,8 @@ namespace GaloDaVelha
 {
 
     /// <summary>
-    /// This enumerator contains all the pieces used in the game
+    /// This enumerator contains all the pieces used in the game as chars
+    /// Used In SetPiece() method
     /// </summary>
     [Flags]
     public enum Pieces
@@ -13,31 +14,31 @@ namespace GaloDaVelha
         Null = 'X' << 0,
 
         //Cant left or right chars or theyll break
-        BigSquare = '\u2610',
+        BigSquare = '\u2610',  //☐
 
 
         //SmallSquare = 1 << 2,
-        SmallSquare = '\u25C7',
+        SmallSquare = '\u25C7', //◇
 
 
         //BigSquareH = 1 << 3,
-        BigSquareH = '\u2612',
+        BigSquareH = '\u2612',  //☒ 
 
         //SmallSquareH = 1 << 4,
-        SmallSquareH ='\u25C8',
+        SmallSquareH ='\u25C8', //◈
         
         //BigCircle = 1 << 5,
-        BigCircle = '\u25CB',
+        BigCircle = '\u25CB', //○
 
         //SmallCircle = 1 << 6,
-        SmallCircle = '\u25E6',
+        SmallCircle = '\u25E6', //◦
 
 
         //BigCircleH = 1 << 7,
-        BigCircleH = '\u25CE',
+        BigCircleH = '\u25CE', //◎
 
         //SmallCircleH = 1 << 8,
-        SmallCircleH = '\u2609',
+        SmallCircleH = '\u2609', //☉
 
     }
 }
